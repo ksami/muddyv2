1f2eece6f8cec78a59bfb2705fcc1fbea5911c1b
ksami <ksami.ihide@gmail.com>
2015-01-28 17:32:38 +0800

    add sprite kinetic

--- client/js/ui.js
+++ client/js/ui.js
@@ -30,6 +30,7 @@ if (Meteor.isClient) {
     }
   });
 
+
   // Event listener for _streamChat
   _streamChat.on('message', function(msg) {
     addMsgToChat(msg);
@@ -37,7 +38,55 @@ if (Meteor.isClient) {
 
   // Scroll to bottom everytime messages added
   addMsgToChat = function(msg) {
+    // Remove <buffer> oldest messages everytime it hits 2*<buffer>
+    var buffer = 100;
+    if( $('#messages li').length >= buffer*2) {
+      $('#messages li').slice(0,buffer).remove();
+    }
+
     $('#messages').append('<li>' + msg.from + ': ' + msg.text + '</li>');
     $('#messages').scrollTop( $('#messages').prop("scrollHeight") );
+  };
+
+  
+  Template.pageGame.rendered = function() {
+
+    // KINETIC //
+
+    var stage = new Kinetic.Stage({
+      container: 'map',
+      width: 578,
+      height: 200
+    });
+    var layer = new Kinetic.Layer();
+
+    var imageObj = new Image();
+    imageObj.onload = function() {
+      var blob = new Kinetic.Sprite({
+        x: 250,
+        y: 40,
+        image: imageObj,
+        animation: 'walk',
+        animations: {
+          walk: [
+            0,0,64,64,
+            64,0,64,64
+          ]
+        },
+        frameRate: 5,
+        frameIndex: 0
+      });
+
+      // add shape to layer
+      layer.add(blob);
+
+      // add layer to stage
+      stage.add(layer);
+
+      // start sprite animation
+      blob.start();
+    };
+
+    imageObj.src = "/walk.png";
   };
 }
